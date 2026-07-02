e14b2495c3de0f61cab9f0127dcb250637aa0aeb
Stephen Smith <stephen.w.smith4@icloud.com>
2026-07-02 17:36:56 -0400

    initial commit of basic SPSC

---
+++ README.md
@@ -0,0 +1,75 @@
+# spsc-queue
+
+## Overview
+A learning project exploring concurrent data structures in Rust. I followed Erik Rigtorp's [_Optimizing a ring buffer for throughput_](https://rigtorp.se/ringbuffer/) adapting the C++ code into Rust.
+
+## Design
+It's a classic ring buffer implementation for a Single Producer, Single Consumer queue (SPSC): heap allocated `Vec` with two `AtomicUsize` indices for read index and write index.
+
+### Producer/Consumer Handles
+I took it one step further to introduce `Producer<T>` and `Consumer<T>` types to let Rust's type system enforce the fact that we can have only one Producer and one Consumer at a time. Failure to do so will break the RingBuffer.
+
+### Cache-Aligned Indices
+Rigtorp uses `alignas(64)` in his struct definitions in C++. I created a type `CacheAligned<T>` to accomplish the same goal. I implemented `Deref`, `DerefMut`, and `From<T>` for `CacheAligned<T>` for easier ergonomics.
+
+### Cached Index Optimization
+Following his lead, I added the cached values to improve performance by reducing `Acquire` atomic reads. Each time we do an `Acquire` load, we save the value in the cached value slot. When checking if the queue is empty or full, we first compare against the cached value, if that indicates full or empty, we then load the real value and confirm it. If it's confirmed, we return an appropriate `None` in the event of popping off an empty queue or `Some(val)` when pushing onto a full queue.
+
+### Full vs Empty — The Wasted Slot
+Like Rigtorp, I don't enforce power of 2 `capacity` and require at least one item is unused to distinguish an empty queue from a full one. You'll notice in `push_back` that we check 
+```
+if next_write_idx == unsafe { *self.read_idx_cached.get() } {
+    ...
+}
+```
+This is our indication of a full queue. In `pop_front` we check
+```
+if read_idx == unsafe { *self.write_idx_cached.get() } {
+    ...
+}
+```
+Which is our indication of an empty queue.
+
+### Memory Ordering
+Because `pop_front` is only ever called by the `Consumer<T>` aka the reader thread, we can load `read_idx` using `Relaxed` memory ordering. Same logic applies to `push_back`, the `Producer<T>` aka writer thread and the load of `write_idx`.
+
+We need to use `Acquire` ordering when we do the check of our `read_idx` or `write_idx` against the other thread's index. This is paired with a `Release` ordering that's used for our index incrementing and subsequent `store` operation.
+
+## Usage
+Create a channel with a defined capacity (in this case 4) using
+```
+let (tx, rx) = spsc_rigtorp::channel(4);
+```
+
+Push something onto the queue using
+```
+// tx.send(val) will return an Option<T>, None means success,
+// Some(val) gets you your value back
+let foo = tx.send(val);
+```
+
+Pop something off the queue using
+```
+// rx.recv() will return None if the queue is empty and Some(val) if there's something
+// to grab.
+let val = rx.recv();
+```
+
+## Performance
+This is something I need to dig into. It will be a good exercise in using criterion and flamegraph.
+
+## Safety
+`RingBuffer<T>` implements `Send` and `Sync` both assuming that `T` is `Send`. The compiler can't auto derive these marker traits because `UnsafeCell` is inherently `!Sync`. This is ok because we're limited to two threads (a `Producer<T>` and a `Consumer<T>`). `Producer<T>` has exclusive access to `write_idx` and `Consumer<T>` has exclusive access to `read_idx`. Synchronization is required in the cases of an empty or full queue, which is discussed in the Memory Ordering section of this README.
+
+`UnsafeCell` and `MaybeUninit` uses are documented with `SAFETY` comments in the source code.
+
+## Limitations
+* Doesn't batch pushes and pops, which could reduce the number of times we have to update the read and write indices.
+* Capacity of 1 will cause a panic due to the wasted slot issue. This could be remedied by enforcing power of two sizes and a bitmask.
+* No `async` support. That would be a fun exercise to port this to the `async` environment and deal with `Waker`
+* Capacity is bounded at initialization. No growth or resizing allowed at this juncture.
+
+## References
+* Erik Rigtorp [_Optimizing a ring buffer for throughput_](https://rigtorp.se/ringbuffer/)
+* Brian Troutwine _Hands On Concurrency With Rust_ (a little dated, but good fundamentals)
+* Mara Bos _Rust Atomics and Locks_
---
+++ src/lib.rs
@@ -0,0 +1,338 @@
+use std::cell::UnsafeCell;
+use std::mem::MaybeUninit;
+use std::ops::{Deref, DerefMut};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+// Rigtorp's C++ definition of `ringbuffer`
+// struct ringbuffer {
+//   std::vector<int> data_;
+//   alignas(64) std::atomic<size_t> readIdx_{0};
+//   alignas(64) std::atomic<size_t> writeIdx_{0};
+
+//   ringbuffer(size_t capacity) : data_(capacity, 0) {}
+// }
+
+/// Returns a tuple of `Producer<T>` and `Consumer<T>`, accepts internal `RingBuffer`
+/// capacity as an argument.
+/// # Panics will `panic!` if `capacity` == 1 due to required dummy slot to distinguish
+/// empty from full (see README)
+pub fn channel<T>(capacity: usize) -> (Producer<T>, Consumer<T>) {
+    RingBuffer::new(capacity)
+}
+
+/// A wrapper around a `Arc<RingBuffer<T>>`, call `.send(val)` to push a value on to
+/// the underlying `RingBuffer`
+pub struct Producer<T> {
+    buffer: Arc<RingBuffer<T>>,
+}
+
+impl<T> Producer<T> {
+    /// Public API for accessing `RingBuffer`s internal `push_back()`
+    pub fn send(&self, value: T) -> Option<T> {
+        self.buffer.push_back(value)
+    }
+
+    /// Convenience method to check if the queue is full before calling `.send(val)`
+    /// Has side-effect of updating the internal cached index values
+    pub fn is_full(&self) -> bool {
+        self.buffer.is_full()
+    }
+}
+
+/// A wrapper around a `Arc<RingBuffer<T>>`, call `.recv()` to pop a value off
+/// the underlying `RingBuffer`
+pub struct Consumer<T> {
+    buffer: Arc<RingBuffer<T>>,
+}
+
+impl<T> Consumer<T> {
+    /// Public API for accessing `RingBuffer`s internal `pop_front()`
+    pub fn recv(&self) -> Option<T> {
+        self.buffer.pop_front()
+    }
+
+    /// Convenience method to check if the queue is empty before calling `.recv()`
+    /// Has side-effect of updating the internal cached index values
+    pub fn is_empty(&self) -> bool {
+        self.buffer.is_empty()
+    }
+}
+
+struct RingBuffer<T> {
+    data: Vec<UnsafeCell<MaybeUninit<T>>>, // `MaybeUninit<T>` is similar to `Option<T>` without safety checks or overhead
+    read_idx: CacheAligned<AtomicUsize>,
+    read_idx_cached: CacheAligned<UnsafeCell<usize>>,
+    write_idx: CacheAligned<AtomicUsize>,
+    write_idx_cached: CacheAligned<UnsafeCell<usize>>,
+    capacity: usize, // avoids repeated calls to `data.len()` during push and pop operations
+}
+
+unsafe impl<T: Send> Send for RingBuffer<T> {}
+unsafe impl<T: Send> Sync for RingBuffer<T> {}
+
+/// Used to guarantee cache alignment to reduce cache coherency traffic
+#[repr(align(64))]
+struct CacheAligned<T>(T);
+
+impl<T> Deref for CacheAligned<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for CacheAligned<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> From<T> for CacheAligned<T> {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> RingBuffer<T> {
+    /// One slot must be reserved for determining if the queue is full
+    /// a capacity of 1 will lead to a persistently full and useless queue
+    pub fn new(capacity: usize) -> (Producer<T>, Consumer<T>) {
+        assert!(
+            capacity > 1,
+            "Capacity of 1 will result in a full and useless RingBuffer"
+        );
+        let data = (0..capacity)
+            .map(|_| UnsafeCell::new(MaybeUninit::uninit()))
+            .collect();
+        let p_buffer = Arc::new(RingBuffer {
+            data,
+            read_idx: AtomicUsize::new(0).into(),
+            read_idx_cached: UnsafeCell::new(0).into(),
+            write_idx: AtomicUsize::new(0).into(),
+            write_idx_cached: UnsafeCell::new(0).into(),
+            capacity,
+        });
+        let c_buffer = Arc::clone(&p_buffer);
+        (Producer { buffer: p_buffer }, Consumer { buffer: c_buffer })
+    }
+
+    // Rigtorp's `push` implementation in C++
+    // bool push(int val) {
+    //     auto const writeIdx = writeIdx_.load(std::memory_order_relaxed);
+    //     auto nextWriteIdx = writeIdx + 1;
+    //     if (nextWriteIdx == data_.size()) {
+    //         nextWriteIdx = 0;
+    //     }
+    //     if (nextWriteIdx == readIdx_.load(std::memory_order_acquire)) {
+    //         return false;
+    //     }
+    //     data_[writeIdx] = val;
+    //     writeIdx_.store(nextWriteIdx, std::memory_order_release);
+    //     return true;
+    // }
+
+    /// push_back accepts a `T` value and writes it to the writer end of the queue
+    /// Returns `None` on success and `Some(value)` if full, giving the caller
+    /// their value back
+    fn push_back(&self, value: T) -> Option<T> {
+        let write_idx = self.write_idx.load(Ordering::Relaxed);
+
+        // if we've hit the end of the ring buffer we will wrap back around to the beginning
+        let next_write_idx = (write_idx + 1) % self.capacity;
+
+        // SAFETY: We know that `read_idx_cached` holds quality data as it's
+        // initialized on `RingBuffer` creation and is updated with atomic load operations.
+        // Use of `UnsafeCell` is primarily to allow interior mutability with a `&self` method.
+        if next_write_idx == unsafe { *self.read_idx_cached.get() } {
+            // SAFETY: see above
+            unsafe { *self.read_idx_cached.get() = self.read_idx.load(Ordering::Acquire) };
+            // SAFETY: see above
+            if next_write_idx == unsafe { *self.read_idx_cached.get() } {
+                // this means the buffer is full and we'll return the value back to the caller
+                // notice that we're comparing `next_write_idx` and not `write_idx`, this is what
+                // determines empty vs full. We will have one "dummy" slot in use to indicate full.
+                return Some(value);
+            }
+        }
+        // `.get()` to return the `*mut MaybeUninit`, then dereference that and call `.write(value)`
+        // SAFETY: We know that we have memory allocated in every vector slot, it may be uninitialized
+        // at this point, but we're writing good data into it
+        unsafe { (*self.data[write_idx].get()).write(value) };
+        // increment the `write_idx`
+        self.write_idx.store(next_write_idx, Ordering::Release);
+        None
+    }
+
+    // Rigtorp's `pop` implementation in C++
+    // bool pop(int &val) {
+    //     auto const readIdx = readIdx_.load(std::memory_order_relaxed);
+    //     if (readIdx == writeIdx_.load(std::memory_order_acquire)) {
+    //         return false;
+    //     }
+    //     val = data_[readIdx];
+    //     auto nextReadIdx = readIdx + 1;
+    //     if (nextReadIdx == data_.size()) {
+    //         nextReadIdx = 0;
+    //     }
+    //     readIdx_.store(nextReadIdx, std::memory_order_release);
+    //     return true;
+    // }
+
+    /// pop_front returns `Some(value)` on success (there was something on the queue)
+    /// to pop and `None` if nothing is there to take
+    pub fn pop_front(&self) -> Option<T> {
+        let read_idx = self.read_idx.load(Ordering::Relaxed);
+        // SAFETY: We know that `write_idx_cached` holds quality data as it's
+        // initialized on `RingBuffer` creation and is updated with atomic load operations.
+        // Use of `UnsafeCell` is primarily to allow interior mutability with a `&self` method.
+        if read_idx == unsafe { *self.write_idx_cached.get() } {
+            // SAFETY: see above
+            unsafe { *self.write_idx_cached.get() = self.write_idx.load(Ordering::Acquire) };
+            // SAFETY: see above
+            if read_idx == unsafe { *self.write_idx_cached.get() } {
+                // this indicates that the queue is empty (see note about full in the `push_back`)
+                // implementation
+                return None;
+            }
+        }
+        // SAFETY: We know that a value is there (the queue isn't empty), which means something
+        // was written to the memory address
+        let val = unsafe { (*(self.data[read_idx].get())).assume_init_read() };
+
+        // calculate the next read index and wrap around if it hits the end of the list.
+        let next_read_idx = (read_idx + 1) % self.capacity;
+        // increment the read index
+        self.read_idx.store(next_read_idx, Ordering::Release);
+        Some(val)
+    }
+
+    /// should only be called by the consumer thread to preserve exclusive access to `read_idx`
+    /// this will be accessed by `Consumer<T>.is_empty()` and `Self::pop_front()`
+    fn is_empty(&self) -> bool {
+        let read_idx = self.read_idx.load(Ordering::Relaxed);
+        // SAFETY: We know that `write_idx_cached` holds quality data as it's
+        // initialized on `RingBuffer` creation and is updated with atomic load operations.
+        // Use of `UnsafeCell` is primarily to allow interior mutability with a `&self` method.
+        if read_idx == unsafe { *self.write_idx_cached.get() } {
+            // SAFETY: see above
+            unsafe { *self.write_idx_cached.get() = self.write_idx.load(Ordering::Acquire) };
+            // SAFETY: see above
+            if read_idx == unsafe { *self.write_idx_cached.get() } {
+                // this indicates that the queue is empty (see note about full in the `push_back`)
+                // implementation
+                return true;
+            }
+        }
+        false
+    }
+
+    /// should only be called by the producer thread to preserve exclusive access to `write_idx`
+    /// this will be accessed by `Producer<T>.is_full()` and `Self::push_back()`
+    fn is_full(&self) -> bool {
+        let write_idx = self.write_idx.load(Ordering::Relaxed);
+
+        // if we've hit the end of the ring buffer we will wrap back around to the beginning
+        let next_write_idx = (write_idx + 1) % self.capacity;
+
+        // SAFETY: We know that `read_idx_cached` holds quality data as it's
+        // initialized on `RingBuffer` creation and is updated with atomic load operations.
+        // Use of `UnsafeCell` is primarily to allow interior mutability with a `&self` method.
+        if next_write_idx == unsafe { *self.read_idx_cached.get() } {
+            // SAFETY: see above
+            unsafe { *self.read_idx_cached.get() = self.read_idx.load(Ordering::Acquire) };
+            // SAFETY: see above
+            if next_write_idx == unsafe { *self.read_idx_cached.get() } {
+                // this means the buffer is full and we'll return the value back to the caller
+                // notice that we're comparing `next_write_idx` and not `write_idx`, this is what
+                // determines empty vs full. We will have one "dummy" slot in use to indicate full.
+                return true;
+            }
+        }
+        false
+    }
+}
+
+impl<T> Drop for RingBuffer<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+
+    #[test]
+    fn test_basic_operations() {
+        let num_writes = 2500;
+        let (tx, rx) = channel(4);
+
+        let wjh = thread::spawn(move || {
+            let mut i = 0;
+            while i < num_writes {
+                match tx.send(format!("[{i}] String Item")) {
+                    Some(n) => {
+                        println!("Failed to push '{n}', trying again...");
+                        thread::yield_now();
+                        continue;
+                    }
+                    None => {
+                        i += 1;
+                    }
+                }
+            }
+            i
+        });
+
+        let rjh = thread::spawn(move || {
+            let mut received = 0;
+            while received < num_writes {
+                if let Some(n) = rx.recv() {
+                    println!("Popped: '{n}'");
+                    received += 1;
+                }
+            }
+            received
+        });
+
+        let write_count = wjh.join().unwrap();
+        let read_count = rjh.join().unwrap();
+        assert_eq!(write_count, read_count);
+    }
+
+    // used for `test_drop_works` and `TestVal` dummy type to ensure values actually get dropped
+    // using this in other tests will cause problems without making some changes
+    static DROP_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    #[derive(Clone)]
+    struct TestVal {}
+
+    impl Drop for TestVal {
+        fn drop(&mut self) {
+            DROP_COUNT.fetch_add(1, Ordering::Relaxed);
+        }
+    }
+
+    #[test]
+    fn test_drop_works() {
+        let vec_size = 100;
+        // build a channel with capacity 100
+        let (tx, _rx) = channel(100);
+        // put my custom type in a vector to ensure heap allocated objects
+        // are actually dropped
+        let holder = vec![TestVal {}; vec_size];
+        // push 50 copies of `holder` into the channel (half the capacity)
+        for _ in 0..50 {
+            tx.send(holder.clone());
+        }
+        // tx and _rx drop here with 50 unconsumed vector values
+        drop(tx);
+        drop(_rx);
+
+        // assert is happening before `holder` is dropped, so those 10 aren't counted in it
+        // We should see 50 vectors with `vec_size` values in each
+        assert_eq!(50 * vec_size, DROP_COUNT.load(Ordering::Relaxed));
+    }
+}
